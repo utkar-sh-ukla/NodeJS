67456e3306929d28c2b14366f236c6a295c34802
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-05 18:50:45 +0530

    response Object

---
+++ 4. Requests & Responses/4.2 Response Object/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    // Set the response header
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<p>hello, ninjas</p>');
+    res.write('<p>hello again, ninjas</p>');
+
+    res.end();
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
