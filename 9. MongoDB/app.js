2e3e9536f36a6726f9e48e9e76303928b602b217
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-13 22:04:02 +0530

    MongoDB

--- 9. MongoDB/app.js
+++ 9. MongoDB/app.js
@@ -7,27 +7,60 @@ const Blog = require('./models/blog');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI =
-  'mongodb+srv://node-user:user%40123@cluster0.7ybpp.mongodb.net/note-tuts?retryWrites=true&w=majority';
+const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
 
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-// routes
+// mongoose & mongo tests
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  })
+
+  blog.save()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5ea99b49b8531f40c0fde689')
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
@@ -42,49 +75,11 @@ app.get('/blogs/create', (req, res) => {
 });
 
 app.get('/blogs', (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
       res.render('index', { blogs: result, title: 'All blogs' });
     })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post('/blogs', (req, res) => {
-  // console.log(req.body);
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 });
---
+++ 9. MongoDB/readme.md
@@ -0,0 +1,8 @@
+#   MongoDB
+
+### Topics Covered
+-   intro to mongodb
+-   mongodb setup with atlas
+-   mongoose
+-   getting & saving data
+-   outputting documents in views
